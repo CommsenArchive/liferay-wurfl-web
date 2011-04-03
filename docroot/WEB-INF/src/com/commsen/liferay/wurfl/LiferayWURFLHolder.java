a421c10dd547f85fa49bb5dd5ad127326bfbb8df
Milen Dyankov <milendyankov@gmail.com>
2011-04-03 08:35:07 +0200

    Changed license form LGPL to GPL to comply with WURFL's license

--- docroot/WEB-INF/src/com/commsen/liferay/wurfl/LiferayWURFLHolder.java
+++ docroot/WEB-INF/src/com/commsen/liferay/wurfl/LiferayWURFLHolder.java
@@ -1,9 +1,10 @@
 /**
  * Copyright (c) COMMSEN International. All rights reserved.
- *	
+ *
+ *
  * This library is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
@@ -11,8 +12,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see http://www.gnu.org/licenses/lgpl.html.
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see http://www.gnu.org/licenses/gpl.html.
  */
 package com.commsen.liferay.wurfl;
 
